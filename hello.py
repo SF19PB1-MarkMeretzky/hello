bccdc6ef1015f8e8f43b9f14c3a002eee89f3a64
Mark Meretzky <mark.meretzky@gmail.com>
2019-08-21 07:44:59 -0400

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,7 +1,9 @@
 """
 hello.py
 
-This python program prints "hello" (followed by an invisible newline character).
+This python program prints "hello", followed by an invisible newline character.
+Then it passes exit status 0 back to the operating system to indicate successful
+termination.
 """
 
 import sys
